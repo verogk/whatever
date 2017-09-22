dc08f4b0236e06b5a9b457e7a00bcde7ad5dfd35
Raghu Betina <raghu@firstdraft.com>
2017-09-22 20:42:28 +0000

    Direct association between movies and boomarks

--- app/models/boomark.rb
+++ app/models/boomark.rb
@@ -1,6 +1,8 @@
 class Boomark < ApplicationRecord
   # Direct associations
 
+  belongs_to :movie
+
   # Indirect associations
 
   # Validations
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ApplicationRecord
   # Direct associations
 
+  has_many   :boomarks,
+             :dependent => :destroy
+
   has_many   :castings,
              :dependent => :destroy
 
