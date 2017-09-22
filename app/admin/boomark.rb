a251a2d255e175dbf201d6022a062234e2495001
Raghu Betina <raghu@firstdraft.com>
2017-09-22 20:43:14 +0000

    Whitelist columns in Boomark for ActiveAdmin

--- app/admin/boomark.rb
+++ app/admin/boomark.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Boomark do
 
+ permit_params :movie_id, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
