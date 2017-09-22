4f5f9395c714544d4d677d32ef012bf891866917
Raghu Betina <raghu@firstdraft.com>
2017-09-22 20:42:28 +0000

    Direct association between movies and directors

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,6 +1,9 @@
 class Director < ApplicationRecord
   # Direct associations
 
+  belongs_to :director,
+             :class_name => "Movie"
+
   # Indirect associations
 
   # Validations
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,10 @@
 class Movie < ApplicationRecord
   # Direct associations
 
+  has_many   :directors,
+             :foreign_key => "director_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
